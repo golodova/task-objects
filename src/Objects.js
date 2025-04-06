7a222fd98ddbbda21796aa500e7d6e79f974cd21
golodova <golodn03@mail.ru>
2025-04-06 17:37:28 +0300

    Задание по объектам

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,16 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+  if (data.gender === 'female') {
+    if ('age' in data) {
+      delete data.age;
+    }
+  } else if (data.gender === 'male') {
+    if (!('income' in data)) {
+      data.income = 100000;
+    }
+  }
+  return data;
 }
 
 /*
@@ -15,6 +25,13 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+  const fields = new Set();
+  
+  [obj1, obj2, obj3].forEach(obj => {
+    Object.keys(obj).forEach(key => fields.add(key));
+  });
+  
+  return Array.from(fields).sort();
 }
 
 /*
@@ -23,4 +40,12 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+  const clones = [];
+  for (let i = 1; i <= count; i++) {
+    // Глубокое копирование объекта
+    const clone = JSON.parse(JSON.stringify(obj));
+    clone.id = i;
+    clones.push(clone);
+  }
+  return clones;
 }
